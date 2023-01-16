06ddeaad9f7f250a0680320e48fc81ca6552950d
jahidul96 <jahidul59895@gmail.com>
2023-01-16 13:22:08 +0600

    added register with node js api code examplegit add .

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/userModel");
 const router = express.Router();
 
 // register a user
 router.post("/register", async (req, res) => {
-  const { username, email, ...rest } = req.body;
-
-  console.log(req.body);
+  const { username, email, password, ...rest } = req.body;
+  const saltRounds = 10;
   try {
+    // const userExists = await User.find({ email });
+
+    // if (userExists) {
+    //   return res.status(403).json({
+    //     message: "user already exists!!",
+    //   });
+    // }
+
+    const hasedPassword = await bcrypt.hash(password, saltRounds);
+    const user = new User({
+      email: email,
+      username: username,
+      password: hasedPassword,
+    });
+    user.password = "";
     res.status(201).json({
       message: "Succes",
-      username,
-      email,
+      user,
     });
   } catch (e) {
     res.status(403).json({
